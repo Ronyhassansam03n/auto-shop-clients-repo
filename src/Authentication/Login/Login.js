fad9143288eb2093ca17e24707f57f7698b894cd
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-24 17:22:00 +0600

    login

--- src/Authentication/Login/Login.js
+++ src/Authentication/Login/Login.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import LoginPic from '../../../src/assets/Login/loginpic.webp'
+
 
 const Login = () => {
     return (
-        <div>
-            this is Login
+        <div className="hero min-h-screen  mb-10">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className="text-center lg:text-left">
+      <h1 className="text-4xl text-center font-bold mb-5">Login now!</h1>
+
+      <img src={LoginPic} alt=''></img>
+
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+      <h1 className="text-2xl font-semibold text-center">Login</h1>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" name='email' className="input input-bordered" />
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" name='password' className="input input-bordered" />
+          <label className="label">
+            <Link to='/signup' className="label-text-alt link link-hover">Create a new account!</Link>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-outline">Login</button>
+
+          
+              <div className="divider">OR</div>
+            <button className="btn btn-outline ">Login Via google</button>
+  
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
--- src/Authentication/SignUp/SignUp.js
+++ src/Authentication/SignUp/SignUp.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SignPic from '../../../src/assets/signup/signpic.gif'
+
 
 const SignUp = () => {
     return (
-        <div>
-            this is sign up
+        <div className="hero min-h-screen  mb-10">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className="text-center lg:text-left">
+      <h1 className="text-4xl font-bold text-center mb-5" >Sign Up now!</h1>
+
+      <img src={SignPic} alt=''></img>
+
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+
+
+      <h1 className="text-2xl font-semibold text-center">Sign Up </h1>
+
+      <div className="form-control">
+          <label className="label">
+            <span className="label-text">Full Name</span>
+          </label>
+          <input type="text" placeholder="email" name='name' className="input input-bordered" />
         </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" name='email' className="input input-bordered" />
+        </div>
+
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" name='password' className="input input-bordered" />
+          <label className="label">
+            <Link to='/login' className="label-text-alt link link-hover">Already have an account!</Link>
+          </label>
+        </div>
+
+
+        <div className="form-control mt-6">
+          <button className="btn btn-outline">Sign Up</button>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
