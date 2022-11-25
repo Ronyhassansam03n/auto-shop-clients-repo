163d82aba061b8bcc2f8988bc60ada980bcbda14
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-26 02:19:31 +0600

    category done

--- src/Card/Category/Category.js
+++ src/Card/Category/Category.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Product from '../Product/Product';
 
 
 const Category = () => {
@@ -8,7 +9,21 @@ const Category = () => {
     console.log(carData)
     return (
         <div>
-            <p>  {carData.length}</p>
+            <h1 className='text-4xl font-bold'> Cars for Resale !</h1>
+
+            <div className=' grid lg: grid-cols-3 gap-20 mt-5 mb-10 '>
+
+
+                {
+                    carData.map(car => <Product
+                        key={car._id}
+                        car={car}
+
+
+                    ></Product>)
+
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Card/Product/Product.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Product = ({ car }) => {
+
+
+    const { name, brands, location, resale, original, year, seller, img } = car;
+
+
+    return (
+        <div>
+
+
+            <div className="card w-96 bg-base-100 shadow-xl">
+
+                <figure className="px-10 pt-10">
+
+                    <img src={img} alt="Shoes" className="rounded-xl" />
+
+                </figure>
+
+                <div className="card-body items-center text-center">
+
+
+                    <h2 className="card-title text-error font-semibold"> {name} </h2>
+
+                    <p> Brand Name :{brands}</p>
+                    <p> Location : {location}</p>
+                    <p> Resale Price : ${resale} </p>
+                    <p> Original Price : ${original}</p>
+                    <p> Used Year : {year}</p>
+                    <p> Seller : {seller}</p>
+
+
+                    <div className="card-actions">
+
+                        <button className="btn btn-black btn-outline">Buy Now</button>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Product;
